99eac48c9bc7443a40695dca02404c42bc577c05
Severin Jarl <Jarl.Severin@bolagsverket.se>
2020-06-07 20:46:23 +0200

    test discordinfo

--- controllers/blobboController.js
+++ controllers/blobboController.js
@@ -684,7 +684,7 @@ exports.getMembersInfo = async (options, message) => {
   console.log('allmembers count: ' + allmembers.length + ' discords:' +newdiscords.length);
   let client = message.channel.client;
   await asyncForMembers(allmembers, async(member, index) => {
-    if (!newdiscords.some(e => e.discordId === member.discordId)) {
+    if (!newdiscords.some(e => e.discordId === member.discordId || e.discordId === member.altOf)) {
       
 		let user = await client.fetchUser('365901367853711391');
 		
