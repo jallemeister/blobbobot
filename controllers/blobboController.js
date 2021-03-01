c3774462c50151556b2d8ff70a4d760804f777d2
Severin Jarl <Jarl.Severin@bolagsverket.se>
2021-03-01 22:26:35 +0100

    discord js 12

--- controllers/blobboController.js
+++ controllers/blobboController.js
@@ -653,12 +653,11 @@ exports.getMembersInfo = async (options, message) => {
 	//	console.log("RROOOLE " + key + " : " + value.name);
 	//});
 	let blabla = await dao.clearInfo();
-  guild.members.fetch().then(console.log
-    /*async value =>{
+  guild.members.fetch().then(async value =>{
     
 		//console.log(key + " : " + value.user.username + " " + value.id + " obj " + value.roles);
 		console.log(value.user.username);
-		let existing = await dao.findMemberByDiscordIdRet(value.id);
+		/*let existing = await dao.findMemberByDiscordIdRet(value.id);
 		let rols = '';
 		let isBot = false;
 		value.roles.forEach(function(value, key){
