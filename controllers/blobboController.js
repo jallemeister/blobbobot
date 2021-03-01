eab2616025c63486e9ab72c0dd0ea2e0f46f410f
Severin Jarl <Jarl.Severin@bolagsverket.se>
2021-03-01 20:32:24 +0100

    loggar members

--- controllers/blobboController.js
+++ controllers/blobboController.js
@@ -653,10 +653,10 @@ exports.getMembersInfo = async (options, message) => {
 	//	console.log("RROOOLE " + key + " : " + value.name);
 	//});
 	let blabla = await dao.clearInfo();
-	guild.members.fetch().then(g =>{
+  let LuMembs = await guild.members.fetch();
     console.log('got them members')
-    console.log(g.members)
-		g.members.forEach(async function(value, key, map) {
+    console.log(LuMembs)
+		LuMembs.forEach(async function(value, key, map) {
 		//console.log(key + " : " + value.user.username + " " + value.id + " obj " + value.roles);
 		console.log(value.user.username);
 		let existing = await dao.findMemberByDiscordIdRet(value.id);
@@ -688,7 +688,7 @@ exports.getMembersInfo = async (options, message) => {
 			}
 		}
 		});
-	});
+	
   console.log('INFO SAVED');
   // let allmembers = await dao.findAllMembersRet();
   // console.log('INFO SAVED2');
